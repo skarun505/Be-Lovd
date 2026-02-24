6ed511b36648e545b5a0770eb1cd74c0ca8579fb
skarun505 <skarunkumar460@gmail.com>
2026-02-24 08:31:44 +0530

    Version new

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 import os
 
-PORT = 8000
+PORT = 8001
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 class Handler(http.server.SimpleHTTPRequestHandler):
